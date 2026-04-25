b41a2f3f2ebe440f7b052b2a23888e5c14f25fab
Muhammad Ahmed Chaudhry <mahmedch@stanford.edu>
2026-04-25 10:34:38 -0700

    cleaned test_dataset.py

--- test_dataset.py
+++ test_dataset.py
@@ -1,25 +1,25 @@
 import os
-import torch
 from torch.utils.data import Dataset
 from PIL import Image
 
-from utils.labels import *
-
 class TestDataset(Dataset):
     """
-    Load images and corresponding labels for
+    Prediction dataset that loads only input images.
     """
     def __init__(self, data_path, transforms=None, size=256):
         """
-        Initialize data set
-        Loads and preprocesses data
-        @param data_path : path to data and labels
-        @param size : size of each xray
+        Initialize dataset.
+        @param data_path : path to image directory
+        @param size : resized image dimension (square)
         """
         if not os.path.exists(data_path):
             raise IOError('Path given for TestDataset {} does not exist...'.format(data_path))
         self.data_path = data_path
-        self.data = os.listdir(data_path)
+        valid_exts = (".png", ".jpg", ".jpeg", ".bmp", ".tif", ".tiff", ".webp")
+        self.data = sorted(
+            fname for fname in os.listdir(data_path)
+            if fname.lower().endswith(valid_exts)
+        )
         self.size = size
 
         self.transforms = transforms
@@ -33,10 +33,9 @@ class TestDataset(Dataset):
 
     def __getitem__(self,idx):
         """
-        Gets data at a certain index
+        Gets image at a certain index.
         @param idx : idx of data desired
         @return xray : xray image at idx
-        @return tensor : tensor of condition codes at idx
         """
         fname = self.data[idx]
         xray = Image.open(os.path.join(self.data_path, fname))
@@ -54,17 +53,3 @@ class TestDataset(Dataset):
         @return name : name of study at idx
         """
         return self.data[idx]
-
-if __name__ == "__main__":
-    from torch.utils.data import DataLoader
-    c = TestDataset('data/test')
-    print(len(c))
-    print(c[0])
-    print(c.at(2))
-    data = DataLoader(c, batch_size=4, shuffle=True)
-    for s in data:
-        print(s[0].shape, s[0], s[0].shape)
-        print(s[1].shape, s[1], s[1].shape)
-        print(s[2].shape, s[2], s[2].shape)
-        print(s[3].shape, s[3], s[3].shape)
-        break
